d836751e375de53c9c253f691b222d06df165b7c
protess <protess@gmail.com>
2023-07-03 14:37:05 +0900

    fix deploy branch

--- slides/src/Presentation.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import Slides from './slides/slides.svelte';
-
-    const partner = ['Svelte', 'Reveal.js'];
-</script>
-
-<Slides/>
--- slides/src/slides/slides.svelte
+++
@@ -1,4 +0,0 @@
-
-<section data-background="#ffffff" data-background-image="https://slide-static.s3.ap-northeast-2.amazonaws.com/k8s-bg.png" data-background-opacity="0.10">
-  <h3 style="color:#4a4a4a">CICD-TUTORIAL</h3>
-</section>
